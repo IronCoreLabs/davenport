f82fbd82ef1706da4c93cf5a7739ebdfe494aa4b
Colt Frederickson <coltfred@gmail.com>
2015-11-25 14:39:26 -0700

    Make batch final

--- src/main/scala/com/ironcorelabs/davenport/db/batch.scala
+++ src/main/scala/com/ironcorelabs/davenport/db/batch.scala
@@ -10,12 +10,12 @@ import scalaz.syntax.foldable._
  * Object that contains batch operations for DB. They have been separated out because they cannot be mixed with
  * `DBProg` operations without first lifting them into a process via `liftToProcess`.
  */
-object batch { //scalastyle:ignore 
+final object batch { //scalastyle:ignore 
   def liftToProcess[A](prog: DBProg[A]): Process[DBOps, DBError \/ A] = Process.eval(prog.run)
 
   /**
    * Create all values in the Foldable F.
    */
-  def createDocs[F[_]](foldable: F[(Key, RawJsonString)])(implicit F: Foldable[F]): Process[DBOps, DBError \/ DBValue] = 
+  def createDocs[F[_]](foldable: F[(Key, RawJsonString)])(implicit F: Foldable[F]): Process[DBOps, DBError \/ DBValue] =
     Process.emitAll(foldable.toList).evalMap { case (key, json) => createDoc(key, json).run }
 }
