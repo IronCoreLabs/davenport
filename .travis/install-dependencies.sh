98b95738773d5ecdd02cdf4c538227880213275f
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2015-12-29 10:17:14 -0700

    Upgrade travis couchbase version to 4.1.0

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# couchbase-server-enterprise_3.1.0-ubuntu12.04_amd64.deb
-export CB_SERVER_VERSION="3.1.0"
+export CB_SERVER_VERSION="4.1.0"
 export CB_SERVER_FILE="couchbase-server-enterprise_${CB_SERVER_VERSION}-ubuntu12.04_amd64.deb"
 export CB_SERVER_URL="http://packages.couchbase.com/releases/${CB_SERVER_VERSION}/${CB_SERVER_FILE}"
 
@@ -34,7 +33,7 @@ curl -v -X POST http://127.0.0.1:8091/nodes/self/controller/settings --data-urle
 curl -v -X POST http://127.0.0.1:8091/node/controller/rename -d hostname=127.0.0.1
 
 # Setup Services
-curl -v -X POST http://127.0.0.1:8091/node/controller/setupServices --data-urlencode "services=kv"
+curl -v -X POST http://127.0.0.1:8091/node/controller/setupServices --data-urlencode "services=kv,n1ql,index"
 
 # Setup bucket memory
 curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=1196
@@ -45,4 +44,8 @@ curl -v -X POST http://127.0.0.1:8091/pools/default/buckets -d flushEnabled=1 -d
 # Setup Administrator username and password
 curl -v -X POST http://127.0.0.1:8091/settings/web --data-urlencode password="cb1234" --data-urlencode username="Administrator" -d port=SAME
 sleep 5
+
+# Setup N1QL
+curl -v -X POST http://127.0.0.1:8093/query/service \
+    -d statement="REATE PRIMARY INDEX defaultidx on default USING GSI"
 
